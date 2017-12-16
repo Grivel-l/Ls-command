5bee77aad64a60612fc4fce72a55f31afecea7b1
Leo Grivel <legrivel@z2r2p4.le-101.fr>
2017-12-16 18:56:45 +0100

    ft_readdir wasn't getting the first dir

--- libft/ft_readdir.c
+++ libft/ft_readdir.c
@@ -6,12 +6,13 @@
 /*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/11 19:30:27 by legrivel     #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/13 20:32:18 by legrivel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/16 18:54:52 by legrivel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	**ft_readdir(char *filename, size_t get_hidden)
 {
@@ -24,7 +25,6 @@ char	**ft_readdir(char *filename, size_t get_hidden)
 	all_dirs[0] = NULL;
 	if ((dir = opendir(filename)) == NULL)
 		return (NULL);
-	dir_content = readdir(dir);
 	while ((dir_content = readdir(dir)) != NULL)
 	{
 		if ((get_hidden) || (!get_hidden && dir_content->d_name[0] != '.'))
