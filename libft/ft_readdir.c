d2c2f8fb5d85d1306d95059f16e765c7e59c7937
Leo Grivel <legrivel@z2r3p1.le-101.fr>
2017-12-26 21:05:14 +0100

    Libft updated

--- libft/ft_readdir.c
+++ libft/ft_readdir.c
@@ -6,7 +6,7 @@
 /*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/11 19:30:27 by legrivel     #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/26 20:20:49 by legrivel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/12/26 20:59:33 by legrivel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -39,5 +39,5 @@ t_list	*ft_readdir(char *filename, size_t get_hidden)
 	}
 	if (closedir(dir) == -1)
 		return (NULL);
-	return (start);
+	return (start == NULL ? ft_lstnew("", 0) : start);
 }
