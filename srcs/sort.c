d7324625587c67c329b4f0c27b4c939993c4be0b
Leo Grivel <legrivel@z2r2p2.le-101.fr>
2018-01-13 17:55:54 +0100

    Alphabetical sort fixed to OSX standards and time sort done

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,7 +6,7 @@
 /*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/26 23:54:59 by legrivel     #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/27 00:11:16 by legrivel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/13 17:54:13 by legrivel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,32 +21,45 @@ void	sort_list(t_flist **list, char *options)
 
 static int	sort_time(t_file *file, t_list *target)
 {
-	(void)file;
-	(void)target;
-	return (0);
+	long		result;
+	struct stat	target_info;
+	char		*target_path;
+
+	if ((target_path = ft_strjoin(file->path, target->content)) == NULL)
+		return (-1);
+	if (stat(target_path, &target_info) == -1)
+		return (-1);
+	ft_strdel(&target_path);
+	result = file->file_info.st_mtimespec.tv_sec - target_info.st_mtimespec.tv_sec;
+	if (result < 0)
+		return (1);
+	else if (result > 0)
+		return (0);
+	else
+		return (2);
 }
 
 static int sort_alpha(t_file *file, t_list *target)
 {
-	return (ft_strcmp_ci(file->filename[0] == '.' ?
-							&(file->filename[1]) : file->filename,
-			((char *)(target->content))[0] == '.' ? &(target->content[1]) : target->content));
+	return (ft_strcmp(file->filename, target->content) < 0 ? 0 : 1);
 }
 
 static int	sort(t_file *file, t_list *target, size_t time_sort)
 {
-	size_t	are_equal;
+	int		are_equal;
 
 	if (ft_strcmp(file->filename, ".") == 0 ||
 		ft_strcmp(file->filename, "..") == 0)
-		return (-1);
+		return (0);
 	if (ft_strcmp(target->content, ".") == 0 ||
 		ft_strcmp(target->content, "..") == 0)
 		return (1);
 	are_equal = 0;
 	if (time_sort)
 		are_equal = sort_time(file, target);
-	if (are_equal == 0)
+	if (are_equal == -1)
+		return (-1);
+	if (are_equal == 2)
 		return (sort_alpha(file, target));
 	else
 		return (are_equal);
@@ -56,11 +69,14 @@ t_flist	*set_flist_link(t_flist **list_start,
 				t_list *files, char *path, char *options)
 {
 	t_flist	*list;
+	int		sort_result;
 
 	list = *list_start;
 	while (list != NULL)
 	{
-		if (sort(list->file, files, ft_strchr(options, 't') != NULL) < 0)
+		if ((sort_result = sort(list->file, files, ft_strchr(options, 't') != NULL)) == -1)
+			return (NULL);
+		else if (sort_result == 0)
 		{
 			if (list->left == NULL)
 			{
