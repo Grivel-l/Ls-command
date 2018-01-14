0a6ad7acea0e780dea0cd2f3737f4719b72cf8a2
Leo Grivel <legrivel@z2r2p4.le-101.fr>
2018-01-14 23:14:45 +0100

    Normed

--- libft/ft_putofft.c
+++ libft/ft_putofft.c
@@ -6,7 +6,7 @@
 /*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/14 23:09:08 by legrivel     #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/14 23:10:05 by legrivel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/14 23:13:53 by legrivel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,13 +15,6 @@
 
 void	ft_putofft(off_t nb)
 {
-// 	if (nb == -2147483648)
-// 	{
-// 		ft_putchar('-');
-// 		ft_putchar(2 + '0');
-// 		ft_putnbr(147483648);
-// 		return ;
-// 	}
 	if (nb >= 0 && nb <= 9)
 		ft_putchar(nb + '0');
 	else if (nb >= 10)
