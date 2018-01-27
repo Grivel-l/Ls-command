bc51f3cb0b2f64b47170d3aef505b9c64cd54d8c
Leo Grivel <legrivel@z2r2p3.le-101.fr>
2018-01-27 16:11:40 +0100

    Norme

--- srcs/print_infos.c
+++ srcs/print_infos.c
@@ -6,7 +6,7 @@
 /*   By: legrivel <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/14 18:58:03 by legrivel     #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/26 03:21:55 by legrivel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/27 16:11:30 by legrivel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -37,7 +37,7 @@ int			print_link(t_file *file)
 	if ((file_path = ft_strjoin(file->path, file->filename)) == NULL)
 		return (-1);
 	ft_memset(buf, 0, sizeof(buf));
-	if (readlink(file_path,buf, PATH_MAX - 1) == -1)
+	if (readlink(file_path, buf, PATH_MAX - 1) == -1)
 	{
 		ft_strdel(&file_path);
 		return (-1);
